290cb3a910d7826c083f9bda00edbb21c7b7bf36
Huzaifa Fiaz <119066287+huzaifaawan699@users.noreply.github.com>
2024-01-16 20:09:45 +0500

    Create RadixSort.cpp

---
+++ RadixSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+void countingSort(int arr[], int n, int exp) {
+    const int base = 10;
+
+    int output[n];
+    int count[base] = {0};
+
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % base]++;
+
+    for (int i = 1; i < base; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % base] - 1] = arr[i];
+        count[(arr[i] / exp) % base]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    radixSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
