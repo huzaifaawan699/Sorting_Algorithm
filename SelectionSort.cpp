958e9c4f8cb8150d3c50e04230d54a76215ac657
Huzaifa Fiaz <119066287+huzaifaawan699@users.noreply.github.com>
2024-01-16 20:10:55 +0500

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+
